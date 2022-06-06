df15cc5a5f91c57b9c80e90dabae3a7698e8d088
Raphael <raphael.lukas1983@gmail.com>
2022-06-06 14:18:33 +0200

    A lot of changes to commit and push

---
+++ SpecialsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza;
+
+[ApiController]
+[Route("specials")]
+public class SpecialsController : Controller
+{
+    private readonly PizzaStoreContext _db;
+
+    public SpecialsController(PizzaStoreContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+    {
+        return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+    }
+}
